fff2fa23f7f466b29a4856282bb413ac9143149d
AmaraaTs <amarjargal.ts01@gmail.com>
2024-07-03 12:16:21 +0800

    calculator ehlew

---
+++ main.js
@@ -0,0 +1,9 @@
+let btnNum = document.getElementsByClassName("btnNum")[0];
+const btnVrjih = document.getElementById("btnVrjih");
+const btnHuwaah = document.getElementById("btnHuwaah");
+const btnNemeh = document.getElementById("btnNemeh");
+const btnTseg = document.getElementById("btnTseg");
+const btnTentsvv = document.getElementById("btnTentsvv");
+const btnHasah = document.getElementById("btnHasah");
+
+console.log(btnNum);
