4dd5a1a2e4aa5bb4908a1c2fd6fc053873bc59e0
AmaraaTs <amarjargal.ts01@gmail.com>
2024-07-05 10:31:47 +0800

    calculator

--- main.js
+++ main.js
@@ -1,9 +1,41 @@
-let btnNum = document.getElementsByClassName("btnNum")[0];
-const btnVrjih = document.getElementById("btnVrjih");
-const btnHuwaah = document.getElementById("btnHuwaah");
-const btnNemeh = document.getElementById("btnNemeh");
-const btnTseg = document.getElementById("btnTseg");
-const btnTentsvv = document.getElementById("btnTentsvv");
-const btnHasah = document.getElementById("btnHasah");
+const btnNum = document.getElementsByClassName("btnNum");
+const btnOperator = document.getElementsByClassName("btnOperator");
+const equal = document.getElementsByClassName("equal")[0];
+const display = document.getElementsByClassName("display")[0];
+const clear = document.getElementsByClassName("ac")[0];
 
 console.log(btnNum);
+
+let operator = "";
+let a = 0;
+
+for (let i = 0; i < btnNum.length; i++) {
+  btnNum[i].addEventListener("click", () => {
+    display.textContent += btnNum[i].textContent;
+  });
+}
+
+for (let i = 0; i < btnOperator.length; i++) {
+  btnOperator[i].addEventListener("click", () => {
+    operator = btnOperator[i].textContent;
+    a = display.textContent;
+    display.textContent = "";
+  });
+}
+
+equal.addEventListener("click", () => {
+  let result = 0;
+  if (operator === "+") {
+    result = Number(a) + Number(display.textContent);
+  } else if (operator === "-") {
+    result = Number(a) - Number(display.textContent);
+  } else if (operator === "*") {
+    result = Number(a) * Number(display.textContent);
+  } else if (operator === "/") {
+    result = Number(a) / Number(display.textContent);
+  }
+
+  display.textContent = result;
+});
+
+clear.addEventListener("click", (display = ""));
